e244f82586122a96ea83e59d68d358e53d7d4bab
Zdenek Grof <grofz@vscht.cz>
2022-12-19 21:38:54 +0100

    Day 19/2 finished. Test case 1 not working.

--- src/day2219.f90
+++ src/day2219.f90
@@ -14,7 +14,7 @@ module day2219_mod
   end interface
 
   integer, parameter :: ID_R=1, ID_C=2, ID_B=3, ID_G=4, ID_ROB=+4
-  integer, parameter :: INV_MAX=8, TIME_MAX=24
+  integer, parameter :: INV_MAX=8, TIME_MAX=32
   character(len=*), parameter :: CH_R='ore', CH_C='clay', CH_B='obsidian', CH_G='geode'
 
   type blueprint_t
@@ -23,6 +23,7 @@ module day2219_mod
   end type
 
   type state_t
+    integer :: deadline=24
     integer :: inv(INV_MAX, TIME_MAX) = 0
     integer :: factory(TIME_MAX) = 0
     type(blueprint_t), pointer :: bp => null()
@@ -36,7 +37,8 @@ contains
     type(string_t), allocatable :: lines(:)
     type(blueprint_t), allocatable, target :: bb(:)
     type(state_t), allocatable :: init(:)
-    integer :: i, j, t, ans
+    integer :: i, j, t, ans, fid
+    integer, parameter :: time_part_1=24, time_part_2=32
 
     lines = read_strings(file)
     !lines = read_strings('inp/19/test.txt')
@@ -45,16 +47,30 @@ contains
       bb(i) = blueprint_new(lines(i)%str)
     end do
 
+    open(newunit=fid, file='test.log', status='replace')
     allocate(init(size(bb)))
     ans = 0 
     do i=1,size(bb)
-      init(i) = state_new(bb(i))
+      init(i) = state_new(bb(i), time_part_1)
       t=3
       call deep_search(init(i), t)
       call state_print(init(i))
-      ans = ans + init(i)%inv(ID_G,TIME_MAX) * init(i)%bp%id
+      ans = ans + init(i)%inv(ID_G,init(i)%deadline) * init(i)%bp%id
+      write (fid,*) init(i)%bp%id, init(i)%inv(ID_G,init(i)%deadline)
     end do
-    print *, ans, ans==1766
+    close(fid)
+    print *, ans, ans==1766 .or. ans==33
+
+    ! Part 2
+    ans = 1
+    do i=1,min(size(bb),3)
+      init(i) = state_new(bb(i), time_part_2)
+      t=3
+      call deep_search(init(i), t)
+      call state_print(init(i))
+      ans = ans * init(i)%inv(ID_G,init(i)%deadline)
+    end do
+    print *, ans, ans==30780 .or. ans==56*62
   end subroutine day2219
 
 
@@ -64,41 +80,33 @@ contains
 
     type(state_t) :: s0, sbest
     integer :: t0, k, tbest
-    logical :: select_factory(4)
+    logical :: select_factory(4), can_build_later(4)
 
-    if (time==TIME_MAX) return
+    if (time==s%deadline) return
 
     do
       select_factory = build_choice(s, time)
 
       if (select_factory(ID_G)) then
+        ! Build geode whenever can be build
         call state_build(s, ID_G, time)
-        time = time + 1
       else if (select_factory(ID_B)) then
+        ! Build obsidian whenever can be build
         call state_build(s, ID_B, time)
-        time = time + 1
       else if (count(select_factory)==0) then
-        time = time + 1
+        continue
       else 
         exit
       end if
 
-      ! no choice, just advance time and try again
-      if (time==TIME_MAX) return
+      ! No recursion yet, just advance time and try again
+      time = time + 1
+      if (time==s%deadline) return
     end do
 
     sbest = s
     tbest = time
 
-    ! try also doing nothing
-    s0 = s
-    t0 = time + 1
-    call deep_search(s0, t0)
-    if (s0%inv(ID_G,TIME_MAX) > sbest%inv(ID_G,TIME_MAX)) then
-      sbest = s0
-      tbest = t0
-       !print *, ' improved  ',s0%inv(ID_G,TIME_MAX), t0
-    end if
 
     do k=4,1,-1
       if (.not. select_factory(k)) cycle
@@ -107,7 +115,7 @@ contains
       call state_build(s0, k, t0)
       t0 = t0 + 1
       call deep_search(s0, t0)
-      if (s0%inv(ID_G,TIME_MAX) > sbest%inv(ID_G,TIME_MAX)) then
+      if (s0%inv(ID_G,s%deadline) > sbest%inv(ID_G,s%deadline)) then
         sbest = s0
         tbest = t0
        !print *, ' improved  ',s0%inv(ID_G,TIME_MAX), t0
@@ -115,22 +123,34 @@ contains
       end if
     end do
 
+    ! try also doing nothing
+    s0 = s
+    t0 = time + 1
+    call deep_search(s0, t0)
+    if (s0%inv(ID_G,s%deadline) > sbest%inv(ID_G,s%deadline)) then
+      sbest = s0
+      tbest = t0
+       !print *, ' improved  ',s0%inv(ID_G,TIME_MAX), t0
+    end if
+
     s = sbest
     time = tbest
   end subroutine deep_search
 
 
-  type(state_t) function state_new(bp) result(new)
-    integer :: t
+  type(state_t) function state_new(bp, deadline) result(new)
+    integer, intent(in) :: deadline
     type(blueprint_t), intent(in), target :: bp
+    integer :: t
 
+    new%deadline = deadline
     new%bp => bp
     new%inv = 0
     new%inv(ID_ROB+ID_R,:) = 1
     new%factory = 0
 
     new%inv(ID_R,1) = new%inv(ID_R+ID_ROB,1)
-    do t=2, TIME_MAX
+    do t=2, new%deadline
       new%inv(ID_R,t) = new%inv(ID_R,t-1) + new%inv(ID_R+ID_ROB,t)
     end do
   end function state_new
@@ -141,7 +161,7 @@ contains
     class(state_t), intent(in) :: th
     integer, intent(in) :: time
 
-    integer :: k, id_robot
+    integer :: k, id_robot, max_needed
 
     can_build = .true.
     do id_robot=1,4
@@ -152,6 +172,15 @@ contains
           exit
         end if
       end do
+    end do
+
+    ! remove choices that are no longer needed
+    do id_robot=1,4
+      ! there is never too many geodes
+      if (id_robot==ID_G) cycle
+      if (.not. can_build(id_robot)) cycle
+      max_needed = maxval(th%bp%cost(id_robot,:),dim=1)
+      if (th%inv(id_robot+ID_ROB,time)>=max_needed) can_build(id_robot) = .false.
     end do
   end function
 
@@ -167,33 +196,42 @@ contains
     do k=1, size(th%bp%cost,1)
       if (th%bp%cost(k, id_robot)==0) cycle
       if (th%inv(k,time-1) < th%bp%cost(k, id_robot)) error stop 'not enough resources'
-      do t = time, TIME_MAX
+      do t = time, th%deadline
         ! add robot to stock in the future
         if (t>time .and. k==1) th%inv(id_robot+ID_ROB,t) = th%inv(id_robot+ID_ROB,t) + 1
 
         ! update resources / pay for building a robot
         th%inv(k,t) = th%inv(k,t) - th%bp%cost(k, id_robot)
-        if (th%inv(k,t)<0) error stop 'not enough resources in the future'
+        if (th%inv(k,t)<0) then
+          call state_print(th)
+          error stop 'not enough resources in the future'
+        end if
       end do
     end do
 
     ! Because of the new robot recalculate its products 
-    do t= time+1, TIME_MAX
+    do t= time+1, th%deadline
       th%inv(id_robot,t) = th%inv(id_robot,t-1) + th%inv(id_robot+ID_ROB,t)
     end do
 
     ! Mark factory
     th%factory(time) = id_robot
-
   end subroutine state_build
 
 
   subroutine state_print(th)
     class(state_t), intent(in) :: th
-    print '(24(i3,1x))', transpose(th%inv)
-    print *, 'blueprint id: ',th%bp%id 
-    print '(24(i3,1x))', th%factory
-    print *
+    if (th%deadline==24) then
+      print '(24(i2,1x))', transpose(th%inv(:,1:th%deadline))
+      print *, 'blueprint id: ',th%bp%id 
+      print '(24(i2,1x))', th%factory(1:th%deadline)
+      print *
+    else
+      print '(32(i2,1x))', transpose(th%inv(:,1:th%deadline))
+      print *, 'blueprint id: ',th%bp%id 
+      print '(32(i2,1x))', th%factory(1:th%deadline)
+      print *
+    end if
   end subroutine state_print
 
 
